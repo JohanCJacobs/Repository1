e8bfe697f75ac67c83d1908defd53e8471e9a69b
Student <Student@ZAPRE-D00000149.DirectDelivery.com>
2017-10-11 11:11:34 +0200

    Added d1 and d2 Cal2 project

--- NetBeansProjects/Cal2/src/cal2/Cal2.java
+++ NetBeansProjects/Cal2/src/cal2/Cal2.java
@@ -17,8 +17,12 @@ public class Cal2 {
     
     public int Add(String s1, String s2)
     {
-        return 0;
+        int d1 = Integer.parseInt(s1);
+        int d2 = Integer.parseInt(s2);
+        
+        return d1 + d2;
     }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
